7fb5c23bcd6348e44c958517b9fd5497ba63af56
去冲浪鸭 <18106987196@163.com>
2021-05-07 10:08:26 +0800

    change: 把 Authorization 加到 Access-Control-Allow-Headers

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ app.all("*", function (req, res, next) {
     res.set({
       "Access-Control-Allow-Credentials": true, //允许后端发送cookie
       "Access-Control-Allow-Origin": req.headers.origin || "*", //任意域名都可以访问,或者基于我请求头里面的域
-      "Access-Control-Allow-Headers": "X-Requested-With,Content-Type", //设置请求头格式和类型
+      "Access-Control-Allow-Headers":
+        "X-Requested-With,Content-Type,Authorization", //设置请求头格式和类型
       "Access-Control-Allow-Methods": "PUT,PATCH,POST,GET,DELETE,OPTIONS", //允许支持的请求方式
       "Content-Type": "application/json; charset=utf-8", //默认与允许的文本格式json和编码格式
     });
